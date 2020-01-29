dd3093697c4e1e7b54a39c0416fe32f9f79fd8c9
Brian Irustia <BIrustia@sofrecom.com.ar>
2020-01-29 15:36:56 -0300

    3,22

--- src/components/WeatherTemperature.js
+++ src/components/WeatherTemperature.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import WeatherIcons from 'react-weathericons';
 
-const WeatherTemperature = () => (
-    <div><span>25°</span></div>
+const icons={
+
+};
+const getWeatherIcon = weatherState =>{
+    return <WeatherIcons neme={} size="2x"/>;
+}
+
+const WeatherTemperature = ({temperature,weatherState}) => (
+
+    <div>
+        {
+        getWeatherIcon(weatherState)
+        }
+        <span>{`${temperature} c°`}</span>
+    </div>
 );
 
 export default WeatherTemperature;
